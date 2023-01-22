add341ccbcc0c7ddc5c0dc95e78bc9e89a52ea2c
Shubhaankar Sharma <66492212+Shubhaankar-Sharma@users.noreply.github.com>
2023-01-21 18:21:49 -0800

    Mid Landing Page

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -1,10 +1,21 @@
 import { Box, ButtonGroup, Flex, Heading, Spacer, Text, useColorMode, IconButton, Img, Center } from "@chakra-ui/react";
 import { useTextC } from "./theme";
-
+import './LandingPage.css';
+import { useAccount } from "wagmi";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
 
 
 const LandingPage = () => {
+    const { address, isConnected } = useAccount();
+    const { openConnectModal } = useConnectModal();
     const textC = useTextC();
+    const handleSignup = () => { 
+        if (!isConnected) {
+            openConnectModal();
+            return;
+        }
+
+    };
     return (
         <Center>
             {/* <img src = "https://cdn.discordapp.com/attachments/1027312951549513821/1066479185809387691/Peersafe_png.png" alt="title" /> */
@@ -16,20 +27,14 @@ const LandingPage = () => {
                     </Flex>
                 </Box> */}
                 <Flex flexDirection='row' alignItems='center' gap='1' marginTop='30%'>
-                    <Heading as='h3' color='white' size='xl' fontFamily='Clash Display'>Store </Heading>
-                    <Heading as='h3' color='white' size='xl' fontFamily='Clash Display'>your data</Heading>
-                    <Heading as='h3' color='white' size='xl' fontFamily='Clash Display'>securely</Heading>
+                    <Heading as='h3' color='white' size='xl' fontFamily='Clash Display'>Store your data securely</Heading>
                 </Flex>
                 <Flex flexDirection='row' alignItems='center' gap='1'>
                     <Heading as='h3' color='grey' size='xl' fontFamily='Clash Display'>with</Heading>
-                    <Heading as='h3' color='grey' size='xl' fontFamily='Clash Display'>decentralized encryption</Heading>
+                    <Heading as='h3' color='grey' size='xl' fontFamily='Clash Display'>decentralized encryption.</Heading>
                 </Flex>
                 <Text color='gray.300' marginTop='20px' maxWidth='490px' align='center' fontWeight='light' fontFamily='Poppins'>Take control of your data privacy and security with PeerSafe, a decentralized encrypted storage platform.</Text>
-                {/* <Flex flexDirection='row' alignItems='center' gap='1'>
-                    <Heading as='h3' color='white'size='xl' fontFamily='Clash Display'>
-                        Who all have access to your data?
-                    </Heading>
-                </Flex> */}
+                <button className="glass-button" fontFamily='Poppins' onClick={handleSignup}>Get Started</button>
             </Flex>
         </Center>  
     );
