096c0e19907b4145ade53199c9b8de21e4a511a2
spongeboi <hi@spongeboi.com>
2023-07-18 13:05:02 +0530

    update abi

--- frontend-v2/src/utils/abi.ts
+++ frontend-v2/src/utils/abi.ts
@@ -1,310 +1,458 @@
 const abi = [
   {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_deployer",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    anonymous: false,
-    inputs: [
+    "anonymous": false,
+    "inputs": [
       {
-        indexed: true,
-        internalType: "address",
-        name: "previousOwner",
-        type: "address",
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
       },
       {
-        indexed: true,
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
     ],
-    name: "OwnershipTransferred",
-    type: "event",
+    "name": "OwnershipTransferred",
+    "type": "event"
   },
   {
-    inputs: [],
-    name: "getCreator",
-    outputs: [
+    "inputs": [],
+    "name": "getCreator",
+    "outputs": [
       {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [],
-    name: "getOwner",
-    outputs: [
+    "inputs": [],
+    "name": "getOwner",
+    "outputs": [
       {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "_vaultOwner",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "_vaultOwner",
+        "type": "address"
+      }
     ],
-    name: "getVault",
-    outputs: [
+    "name": "getVault",
+    "outputs": [
       {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "string",
-        name: "_userName",
-        type: "string",
+        "internalType": "string",
+        "name": "_userName",
+        "type": "string"
       },
       {
-        internalType: "bytes32",
-        name: "_hashedMessage",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
       },
       {
-        internalType: "uint8",
-        name: "_v",
-        type: "uint8",
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
       },
       {
-        internalType: "bytes32",
-        name: "_r",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
       },
       {
-        internalType: "bytes32",
-        name: "_s",
-        type: "bytes32",
-      },
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
+      }
     ],
-    name: "deploy",
-    outputs: [
+    "name": "deploy",
+    "outputs": [
       {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    stateMutability: "nonpayable",
-    type: "function",
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "string",
-        name: "name",
-        type: "string",
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
       },
       {
-        internalType: "string",
-        name: "fileType",
-        type: "string",
+        "internalType": "string",
+        "name": "fileType",
+        "type": "string"
       },
       {
-        internalType: "string",
-        name: "ipfsHash",
-        type: "string",
+        "internalType": "string",
+        "name": "ipfsHash",
+        "type": "string"
       },
       {
-        internalType: "string",
-        name: "key",
-        type: "string",
+        "internalType": "string",
+        "name": "key",
+        "type": "string"
       },
       {
-        internalType: "bytes32",
-        name: "_hashedMessage",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
       },
       {
-        internalType: "uint8",
-        name: "_v",
-        type: "uint8",
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
       },
       {
-        internalType: "bytes32",
-        name: "_r",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
       },
       {
-        internalType: "bytes32",
-        name: "_s",
-        type: "bytes32",
-      },
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
+      }
     ],
-    name: "createFile",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "createFile",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "bytes32",
-        name: "_hashedMessage",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
       },
       {
-        internalType: "uint8",
-        name: "_v",
-        type: "uint8",
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
       },
       {
-        internalType: "bytes32",
-        name: "_r",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
       },
       {
-        internalType: "bytes32",
-        name: "_s",
-        type: "bytes32",
-      },
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
+      }
     ],
-    name: "getAllFiles",
-    outputs: [
+    "name": "getAllFiles",
+    "outputs": [
       {
-        components: [
+        "components": [
           {
-            internalType: "string",
-            name: "_name",
-            type: "string",
+            "internalType": "string",
+            "name": "_name",
+            "type": "string"
           },
           {
-            internalType: "string",
-            name: "_fileType",
-            type: "string",
+            "internalType": "string",
+            "name": "_fileType",
+            "type": "string"
           },
           {
-            internalType: "string",
-            name: "_ipfsHash",
-            type: "string",
+            "internalType": "string",
+            "name": "_ipfsHash",
+            "type": "string"
           },
           {
-            internalType: "string",
-            name: "_key",
-            type: "string",
-          },
+            "internalType": "string",
+            "name": "_key",
+            "type": "string"
+          }
         ],
-        internalType: "struct File[]",
-        name: "",
-        type: "tuple[]",
-      },
+        "internalType": "struct File[]",
+        "name": "",
+        "type": "tuple[]"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "bytes32",
-        name: "_hashedMessage",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
       },
       {
-        internalType: "uint8",
-        name: "_v",
-        type: "uint8",
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
       },
       {
-        internalType: "bytes32",
-        name: "_r",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
       },
       {
-        internalType: "bytes32",
-        name: "_s",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
       },
       {
-        internalType: "string",
-        name: "ipfsHash",
-        type: "string",
-      },
+        "internalType": "string",
+        "name": "ipfsHash",
+        "type": "string"
+      }
     ],
-    name: "deleteFile",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "deleteFile",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "adminAllVaults",
-    outputs: [
+    "inputs": [
       {
-        internalType: "address[]",
-        name: "",
-        type: "address[]",
-      },
-      {
-        internalType: "contract Vault[]",
-        name: "",
-        type: "address[]",
-      },
+        "internalType": "address",
+        "name": "_user",
+        "type": "address"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
-    constant: true,
+    "name": "getPubKey",
+    "outputs": [
+      {
+        "internalType": "bytes",
+        "name": "",
+        "type": "bytes"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "bytes32",
-        name: "_hashedMessage",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
       },
       {
-        internalType: "uint8",
-        name: "_v",
-        type: "uint8",
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
       },
       {
-        internalType: "bytes32",
-        name: "_r",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
       },
       {
-        internalType: "bytes32",
-        name: "_s",
-        type: "bytes32",
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
       },
       {
-        internalType: "address[]",
-        name: "_users",
-        type: "address[]",
+        "internalType": "address",
+        "name": "_to",
+        "type": "address"
       },
       {
-        internalType: "address[]",
-        name: "_oldVaults",
-        type: "address[]",
+        "internalType": "string",
+        "name": "_fileHash",
+        "type": "string"
       },
+      {
+        "internalType": "string",
+        "name": "_keyHash",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_fileType",
+        "type": "string"
+      }
     ],
-    name: "adminMigrate",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "name": "sendShareRequest",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
+  {
+    "inputs": [],
+    "name": "getShareRequests",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "address",
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "internalType": "string",
+            "name": "_fileHash",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_keyHash",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_name",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_fileType",
+            "type": "string"
+          }
+        ],
+        "internalType": "struct ShareRequest[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "string",
+        "name": "_fileHash",
+        "type": "string"
+      }
+    ],
+    "name": "acceptShareRequest",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "_hashedMessage",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "uint8",
+        "name": "_v",
+        "type": "uint8"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "_r",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "_s",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "string",
+        "name": "_fileHash",
+        "type": "string"
+      }
+    ],
+    "name": "rejectShareRequest",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "adminAllVaults",
+    "outputs": [
+      {
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+      },
+      {
+        "internalType": "address[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
 ];
 
 export default abi;
