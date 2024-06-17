bb5500e4f1f1f13967481f5e65ffb443b609c2c2
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-06-17 16:53:00 +0530

    refactor: simplified & reduced a lot of the code, and refactored the config, and web3auth connector creator

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -4,6 +4,7 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import { CHAIN_NAMESPACES, UX_MODE, WEB3AUTH_NETWORK } from "@web3auth/base";
 import { type Chain } from "wagmi/chains";
+import { type CreateConfigParameters } from "wagmi";
 import { webSocket, createConfig, createStorage, cookieStorage } from "wagmi";
 import { baseSepolia } from "wagmi/chains";
 
@@ -30,74 +31,73 @@ const iconUrl = "/logo192.png"; // change this
 
 const clientId = process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID!;
 
-function Web3AuthConnectorInstances(
-  chains: Chain[],
+function createWeb3AuthInstances(
+  chain: Chain,
   providers: LOGIN_PROVIDERS[],
+  extraParams: { login_hint?: string } = {},
 ) {
-  if (!chains[0]) throw new Error("No chain provided");
-
-  const chainConfig = {
-    chainNamespace: CHAIN_NAMESPACES.EIP155,
-    chainId: "0x" + chains[0].id.toString(16),
-    // This is the public RPC we have added, please pass on your own endpoint while creating an app
-    rpcTarget: chains[0].rpcUrls.default.http[0]!,
-    displayName: chains[0].name,
-    tickerName: chains[0].nativeCurrency?.name,
-    ticker: chains[0].nativeCurrency?.symbol,
-    blockExplorerUrl: chains[0].blockExplorers?.default.url[0],
-  };
-
-  const privateKeyProvider = new EthereumPrivateKeyProvider({
-    config: { chainConfig },
-  });
+  if (!chain) throw new Error("No chain provided");
 
   const web3AuthInstance = new Web3AuthNoModal({
     clientId,
-    privateKeyProvider,
+    privateKeyProvider: new EthereumPrivateKeyProvider({
+      config: {
+        chainConfig: {
+          chainNamespace: CHAIN_NAMESPACES.EIP155,
+          chainId: "0x" + chain.id.toString(16),
+          // This is the public RPC we have added, please pass on your own endpoint while creating an app
+          rpcTarget: chain.rpcUrls.default.http[0]!,
+          displayName: chain.name,
+          tickerName: chain.nativeCurrency?.name,
+          ticker: chain.nativeCurrency?.symbol,
+          blockExplorerUrl: chain.blockExplorers?.default.url[0],
+        },
+      },
+    }),
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET, // change this
   });
 
-  // Add openlogin adapter for customizations
-  const openloginAdapterInstance = new OpenloginAdapter({
-    adapterSettings: {
-      network: "sapphire_devnet",
-      uxMode: UX_MODE.REDIRECT,
-      whiteLabel: {
-        appName: name,
-        logoLight: iconUrl,
-        logoDark: iconUrl,
-        defaultLanguage: "en",
-        mode: "dark",
+  web3AuthInstance.configureAdapter(
+    new OpenloginAdapter({
+      adapterSettings: {
+        network: "sapphire_devnet",
+        uxMode: UX_MODE.REDIRECT,
+        whiteLabel: {
+          appName: name,
+          logoLight: iconUrl,
+          logoDark: iconUrl,
+          defaultLanguage: "en",
+          mode: "dark",
+        },
       },
-    },
-  });
-
-  web3AuthInstance.configureAdapter(openloginAdapterInstance);
+    }),
+  );
 
   return providers.map((provider) =>
     Web3AuthConnector({
       web3AuthInstance,
       loginParams: {
         loginProvider: provider,
+        extraLoginOptions: extraParams,
       },
     }),
   );
 }
 
-export const connectorNames: LOGIN_PROVIDERS[] = [
+export const socialProviders: LOGIN_PROVIDERS[] = [
   "google",
   "github",
   "discord",
 ];
 
-const web3authConnectors = Web3AuthConnectorInstances(
-  [baseSepolia],
-  connectorNames,
-);
+const socialConnectors = createWeb3AuthInstances(baseSepolia, socialProviders);
 
-export const wagmiConfig = createConfig({
+const commonConfig: CreateConfigParameters<
+  readonly [Chain, ...Chain[]],
+  Record<number, ReturnType<typeof webSocket>>
+> = {
   chains: [baseSepolia],
-  connectors: web3authConnectors,
+  connectors: socialConnectors,
   transports: {
     [baseSepolia.id]: webSocket(), // put alchemy websocket url here
   },
@@ -105,7 +105,17 @@ export const wagmiConfig = createConfig({
   storage: createStorage({
     storage: cookieStorage,
   }),
-});
+};
+
+export const wagmiConfig = createConfig(commonConfig);
+
+export const emailWagmiConfig = (email: string) =>
+  createConfig({
+    ...commonConfig,
+    connectors: createWeb3AuthInstances(baseSepolia, ["email_passwordless"], {
+      login_hint: email,
+    }),
+  });
 
 declare module "wagmi" {
   interface Register {
