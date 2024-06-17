815a427c1af1ef93ef5adf6e624ebd662b623209
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-06-17 14:29:33 +0530

    feat(mobile-nav): added a share requests tab
    changed buttons into links

--- src/components/nav/bottom.tsx
+++ src/components/nav/bottom.tsx
@@ -3,22 +3,31 @@
 import { usePathname } from "next/navigation";
 import Home from "../icons/home";
 import Settings from "../icons/settings";
+import Users from "../icons/users";
+import Link from "next/link";
 
 const BottomNav = () => {
   const pathname = usePathname();
 
   return (
     <nav className="fixed bottom-0 flex h-16 w-full items-center justify-around border-t bg-background/75 sm:hidden">
-      <button className="">
+      <Link href="/vault">
         <Home className={pathname === "/vault" ? "" : "stroke-foreground/60"} />
-      </button>
-      <button>
+      </Link>
+      <Link href="/vault/requests">
+        <Users
+          className={
+            pathname === "/vault/requests" ? "" : "stroke-foreground/60"
+          }
+        />
+      </Link>
+      <Link href="/vault/settings">
         <Settings
           className={
             pathname === "/vault/settings" ? "" : "stroke-foreground/60"
           }
         />
-      </button>
+      </Link>
     </nav>
   );
 };
