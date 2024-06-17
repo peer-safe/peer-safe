7962eb7d57b68760499a823da40283564ba1baa9
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-06-17 17:05:24 +0530

    feat(login): added email login option

--- src/app/(landing)/login/page.tsx
+++ src/app/(landing)/login/page.tsx
@@ -2,13 +2,15 @@
 
 import { QuestionMarkIcon } from "@radix-ui/react-icons";
 import { redirect } from "next/navigation";
+import { useState, type FC } from "react";
 import { type Connector, useAccount, useConnect } from "wagmi";
 import Discord from "~/components/icons/discord";
 import Github from "~/components/icons/github";
 import Google from "~/components/icons/google";
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 import { Spinner } from "~/components/ui/spinner";
-import { connectorNames } from "~/lib/wagmi";
+import { emailWagmiConfig, socialProviders } from "~/lib/wagmi";
 
 function ProviderLoginButton({
   connector,
@@ -38,25 +40,33 @@ function ProviderLoginButton({
   );
 }
 
-const icons: Record<string, React.FC> = {
+const icons: Record<string, FC> = {
   google: Google,
   github: Github,
   discord: Discord,
+  default: QuestionMarkIcon,
 };
 
 export default function LoginPage() {
   const { connectors } = useConnect();
-  const { status } = useAccount();
+  const { status, isConnecting } = useAccount();
+
+  const [email, setEmail] = useState("");
+
+  const { connect, connectors: emailConnectors } = useConnect({
+    config: emailWagmiConfig(email),
+  });
 
   if (status === "connected") redirect("/");
 
   return (
-    <main className="flex w-full grow flex-col items-center justify-center px-10 py-24">
+    <main className="flex w-full grow flex-col items-center justify-center gap-6 px-10 py-24">
       <div className="text-3xl font-semibold">Welcome back</div>
-      <div className="text-gray-500">Choose how you want to proceed</div>
-      <div className="flex w-full max-w-96 items-center gap-4 pt-6">
+      <div className="-mt-6 text-gray-500">Choose how you want to proceed</div>
+      <div className="flex w-full max-w-96 items-center gap-4">
         {connectors.map((connector, index) => {
-          const Icon = icons[connectorNames[index] || ""] || QuestionMarkIcon;
+          const Icon: FC<{ width: number; height: number }> =
+            icons[socialProviders[index] ?? "default"]!;
 
           return connector.id === "web3auth" ? (
             <ProviderLoginButton connector={connector} key={index}>
@@ -65,6 +75,28 @@ export default function LoginPage() {
           ) : null;
         })}
       </div>
+      <Input
+        placeholder="you@yourdomain.xyz"
+        className="max-w-96"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button
+        className="w-full max-w-96"
+        disabled={
+          isConnecting ||
+          !/^[a-zA-Z0-9\._\+-]+@([a-zA-Z0-9\-]+\.){1,}([a-zA-Z]{2,16})$/.test(
+            email,
+          )
+        }
+        onClick={() => connect({ connector: emailConnectors[0]! })}
+      >
+        {isConnecting ? (
+          <Spinner color="hsl(var(--background))" className="h-5 w-5" />
+        ) : (
+          <>Continue with Email &rarr;</>
+        )}
+      </Button>
     </main>
   );
 }
