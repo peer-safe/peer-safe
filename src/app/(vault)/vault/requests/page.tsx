0f3a95ed8dfd22fb27c5dc54704119c11db6fb2b
pybash1 <67195650+pybash1@users.noreply.github.com>
2024-06-22 11:56:46 +0530

    fix: removed wip icons from vault pages

--- src/app/(vault)/vault/requests/page.tsx
+++ src/app/(vault)/vault/requests/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import WorkInProgress from "~/components/icons/wip";
-
 export default function RequestsPage() {
   return (
     <main className="flex flex-1 flex-col justify-center gap-2 text-center text-xl text-muted-foreground">
       SHARE REQUESTS
-      <WorkInProgress className="flex h-60 text-muted" />
     </main>
   );
 }
--- src/app/(vault)/vault/settings/page.tsx
+++ src/app/(vault)/vault/settings/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import WorkInProgress from "~/components/icons/wip";
-
 export default function SettingsPage() {
   return (
     <main className="flex flex-1 flex-col justify-center gap-2 text-center text-xl text-muted-foreground">
       SETTINGS
-      <WorkInProgress className="flex h-60 text-muted" />
     </main>
   );
 }
