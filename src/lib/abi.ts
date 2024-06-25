54743df3d00692f0e737c0c3f0fcb7c96be23cbb
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2024-06-25 17:00:45 +0530

    feat: signature function using viem

---
+++ src/lib/abi.ts
@@ -0,0 +1,475 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_deployer",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "_fileHash",
+        type: "string",
+      },
+    ],
+    name: "acceptShareRequest",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "adminAllVaults",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "fileType",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "ipfsHash",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "key",
+        type: "string",
+      },
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+    ],
+    name: "createFile",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "ipfsHash",
+        type: "string",
+      },
+    ],
+    name: "deleteFile",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_userName",
+        type: "string",
+      },
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+    ],
+    name: "deploy",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+    ],
+    name: "getAllFiles",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "_name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "_fileType",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "_ipfsHash",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "_key",
+            type: "string",
+          },
+          {
+            internalType: "address",
+            name: "_sharedBy",
+            type: "address",
+          },
+        ],
+        internalType: "struct File[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getCreator",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_user",
+        type: "address",
+      },
+    ],
+    name: "getPubKey",
+    outputs: [
+      {
+        internalType: "bytes",
+        name: "",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+    ],
+    name: "getShareRequests",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "_from",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "_fileHash",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "_keyHash",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "_name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "_fileType",
+            type: "string",
+          },
+        ],
+        internalType: "struct ShareRequest[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_vaultOwner",
+        type: "address",
+      },
+    ],
+    name: "getVault",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "_fileHash",
+        type: "string",
+      },
+    ],
+    name: "rejectShareRequest",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_hashedMessage",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "_to",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_fileHash",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_keyHash",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_fileType",
+        type: "string",
+      },
+    ],
+    name: "sendShareRequest",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,20 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import {
+  getContract,
+  createWalletClient,
+  custom,
+  toFunctionSelector,
+  keccak256,
+  toHex,
+  toBytes,
+  parseSignature,
+  hexToNumber,
+} from "viem";
+import { baseSepolia } from "viem/chains";
+import { abi } from "./abi";
+
+const CONTRACT_ADDRESS = "0x4FFDE33f6bca791adca8D5194eC8C2934D251f54";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,4 +25,34 @@ export function formatBytes(a: number, b = 2) {
   const c = 0 > b ? 0 : b,
     d = Math.floor(Math.log(a) / Math.log(1024));
   return `${parseFloat((a / Math.pow(1024, d)).toFixed(c))} ${["Bytes", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"][d]}`;
+}
+
+export async function signMesssage(message: string, addy?: boolean) {
+  const client = createWalletClient({
+    chain: baseSepolia,
+    transport: custom(window.ethereum),
+  });
+  const contract = getContract({
+    address: CONTRACT_ADDRESS,
+    abi,
+    client,
+  });
+  const [address] = await client.getAddresses();
+  if (addy) {
+    return { address, contract };
+  }
+
+  toFunctionSelector("function ownerOf(uint256 tokenId)");
+  const messageHash = keccak256(toHex(message));
+  const messageHashBytes = toBytes(messageHash);
+
+  const sig = await client.signMessage({
+    account: address!,
+    message: messageHash,
+  });
+
+  const { r, s } = parseSignature(sig);
+  const v = hexToNumber(`0x${sig.slice(130)}`);
+
+  return { contract, messageHash, messageHashBytes, v, r, s };
 }
